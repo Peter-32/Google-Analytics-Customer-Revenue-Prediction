b4e86175b14c6bc8934cbe55f8b18c435337461b
Peter-32 <peter@impactradius.com>
2018-09-24 21:52:33 -0700

    a

--- main.py
+++ main.py
@@ -23,21 +23,77 @@ from pandasql import sqldf
 
 q = lambda q: sqldf(q, globals())
 
-# Prepare Data
+# ETL
+
+JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
+df = pd.read_csv('/Users/peterjmyers/Work/Google-Analytics-Customer-Revenue-Prediction/data/train.csv',
+                 converters={column: json.loads for column in JSON_COLUMNS},
+                 dtype={'fullVisitorId': 'str'},
+                 nrows=5)
+df.dtypes
+
+# df = df['totals'].apply(lambda x: json_normalize(x))
+# df
+
+
+
+for i in range()
+json_normalize(df['totals'][i])
+df['totals'][0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def load_df(csv_path='/Users/peterjmyers/Work/Google-Analytics-Customer-Revenue-Prediction/data/train.csv', nrows=None):
     JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
 
     df = pd.read_csv(csv_path,
                      converters={column: json.loads for column in JSON_COLUMNS},
-                     dtype={'fullVisitorId': 'str'}, # Important!!
+                     dtype={'fullVisitorId': 'str'},
                      nrows=nrows)
 
     for column in JSON_COLUMNS:
+        print(column)
         column_as_df = json_normalize(df[column])
         column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
         df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
     print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")
     return df
 
-load_df()
+
+# pd.read_csv('/Users/peterjmyers/Work/Google-Analytics-Customer-Revenue-Prediction/data/train.csv', nrows=5)
+
+
+df = load_df(nrows=5)
+df
+
+
+df = pd.DataFrame(load_df(nrows=5)['totals.totals'])
+
+df.columns
+
+
+df['totals.totals']
+
+
+column_as_df = json_normalize(df['totals.totals'])
+#column_as_df['totals'] = column_as_df['totals.totals']
+column_as_df
+
+
+column = "totals.totals"
+column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
+column_as_df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
+column_as_df
